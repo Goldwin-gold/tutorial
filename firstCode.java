ece7d8d3083e5c6604d7e927ac65aa28816785a7
Goldwin <rassgoldwin@gmail.com>
2025-12-24 10:22:08 +0530

    Second Commit

--- firstCode.java
+++ firstCode.java
@@ -1,5 +1,10 @@
+import java.util.*;
 class firstCode{
     public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
         System.out.println("Hello world");
+        System.out.println("Enter your name");
+        String name=sc.nextLine();
+        System.out.println("Your name "+name);
     }
 }
